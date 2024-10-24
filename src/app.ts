4769aa4aca3d4b015911379bd9a4014fee32c121
lukasz-mac <luka.koscik@gmail.com>
2024-10-24 13:21:03 +0200

    add express session with prisma & passport with local strategy to app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,55 @@
 import "dotenv/config";
 import express from "express";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import prisma from "./config/prisma-config.js";
+import { User as UserPrisma, Prisma } from "@prisma/client";
+import passport from "passport";
+import localStrategy from "./config/local-strategy-config.js";
+
+declare global {
+  namespace Express {
+    interface User extends UserPrisma {}
+  }
+}
 
 const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 app.use((_req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL!);
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
+});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const expressStore = expressSession({
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+  }),
+  secret: process.env.SESSION_SECRET!,
+  resave: true,
+  saveUninitialized: true,
+});
+
+app.use(expressStore);
+passport.use(localStrategy);
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  return done(null, user.userId);
+});
+
+passport.deserializeUser(async (userId: number, done) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      userId: userId,
+    },
+  });
+
+  return user ? done(null, user) : done(null);
 });
 
 const PORT = process.env.PORT || 3000;
