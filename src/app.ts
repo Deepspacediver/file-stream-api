058345e877d3c738b7f1be4f6e57d99b2b1306bb
lukasz-mac <luka.koscik@gmail.com>
2024-10-24 10:05:13 +0200

    manually set headers & remove cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL!);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 
