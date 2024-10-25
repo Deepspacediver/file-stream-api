19d360b297c98b399d5cd6799cb5454f7180ffc9
lukasz-mac <luka.koscik@gmail.com>
2024-10-25 10:22:41 +0200

    update register schema

--- src/schema/register-schema.ts
+++ src/schema/register-schema.ts
@@ -8,16 +8,16 @@ const PasswordSchema = z.coerce
   .string({
     message: "Password is required",
   })
-  .min(14, { message: "Password must be at least 14 characters long" })
-  .refine((val) => CONTAINS_NUMBER_REGEX.test(val), {
-    message: "Password must contain a number",
-  })
-  .refine((val) => CONTAINS_UPPER_CASE_REGEX.test(val), {
-    message: "Password must contain an upper case letter",
-  })
-  .refine((val) => CONTAINS_LOWER_CASE_REGEX.test(val), {
-    message: "Password must contain a lower case letter",
-  });
+  .min(6, { message: "Password must be at least 6 characters long" })
+  .regex(CONTAINS_NUMBER_REGEX, "Password must contain a number")
+  .regex(
+    CONTAINS_UPPER_CASE_REGEX,
+    "Password must contain an upper case letter"
+  )
+  .regex(
+    CONTAINS_LOWER_CASE_REGEX,
+    "Password must contain a lower case letter"
+  );
 
 export const RegisterSchema = z.object({
   body: z.object({
@@ -25,6 +25,6 @@ export const RegisterSchema = z.object({
       .string({ message: "Username is required" })
       .min(5, { message: "Username must be at least 5 characters long" })
       .max(20, { message: "Username cannot be longer than 20 characters" }),
+    password: PasswordSchema,
   }),
-  password: PasswordSchema,
 });
