7e4f04159fb6e75fbe47fec73be5390ecf4c6326
lukasz-mac <luka.koscik@gmail.com>
2024-10-25 10:21:34 +0200

    fix headers warning

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -9,11 +9,14 @@ export const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   if (err instanceof ZodError) {
     const zodErrors = getZodErrors(err.issues);
     res.status(400).json({ error: zodErrors });
+    return;
   }
 
   if (err instanceof Error) {
     res.status(400).json({ error: err.message });
+    return;
   }
 
   res.status(500).json({ error: "Internal server error" });
+  return;
 };
