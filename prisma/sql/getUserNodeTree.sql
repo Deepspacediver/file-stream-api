3df80aabbf8ad28e680ae93029917bd7bd923d68
lukasz-mac <luka.koscik@gmail.com>
2024-10-29 09:45:55 +0100

    add recursive query to fetch all folders, subfolders & fils in hierarchical structure

---
+++ prisma/sql/getUserNodeTree.sql
@@ -0,0 +1,22 @@
+WITH RECURSIVE user_nodes AS (
+  SELECT 
+    node_id, 
+    parent_node_id,
+    name 
+  FROM 
+    "nodes"
+   WHERE 
+    user_id = 9 AND node_id = 5 
+  UNION 
+    SELECT 
+      r_nodes.node_id,
+      r_nodes.parent_node_id,
+      r_nodes.name 
+    FROM 
+      "nodes" r_nodes 
+    INNER JOIN user_nodes r_nodes1 
+      ON r_nodes.parent_node_id = r_nodes1.node_id
+) SELECT
+  *
+FROM 
+  user_nodes;
