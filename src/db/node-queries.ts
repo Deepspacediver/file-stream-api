96e7e18c898c5f0bc1a51c7f15ca6cc7bf06069c
lukasz-mac <luka.koscik@gmail.com>
2024-10-31 18:34:34 +0100

    add methods to create node share link & return node tree from node with shared link

--- src/db/node-queries.ts
+++ src/db/node-queries.ts
@@ -1,8 +1,9 @@
-import { queryUserNodes } from "@prisma/client/sql";
+import { queryUserNodes, queryNodesFromNodeId } from "@prisma/client/sql";
 import { CreateNode, Node, NodeWithSubNodes } from "../types/node-types.js";
 import prisma from "../config/prisma-config.js";
 import transfromNodesToFolderTree from "../helpers/transform-to-folder-tree.js";
 import { NodeType } from "@prisma/client";
+import { randomUUID } from "crypto";
 
 export const getNodeTree = async (
   userId: number
@@ -34,4 +35,53 @@ export const createNode = async (data: CreateNode) => {
     },
   });
   return createdNode;
+};
+
+export const createSharedNode = async (
+  nodeIdToShare: number,
+  expiryDate: Date
+) => {
+  const nodeToBeShared = await prisma.node.findFirst({
+    where: {
+      nodeId: nodeIdToShare,
+    },
+  });
+
+  if (nodeToBeShared?.type === NodeType.FILE) {
+    throw new Error("Only folders can be shared");
+  }
+
+  const linkHash = randomUUID();
+  const sharedNode = await prisma.nodeShareLink.create({
+    data: {
+      sharedNodeId: nodeIdToShare,
+      linkHash,
+      expiryDate,
+    },
+  });
+  const link = `${process.env.CLIENT_URL}/share/${sharedNode.linkHash}`;
+  return link;
+};
+
+export const getSharedNodeTree = async (linkHash: string) => {
+  const nodeLink = await prisma.nodeShareLink.findFirst({
+    where: {
+      linkHash,
+    },
+  });
+  if (!nodeLink) {
+    throw new Error("Folder with given link does not exists");
+  }
+  if (nodeLink.expiryDate < new Date()) {
+    throw new Error("Link has expired");
+  }
+  const nodes = (await prisma.$queryRawTyped(
+    queryNodesFromNodeId(nodeLink.sharedNodeId)
+  )) as Node[] | null;
+
+  if (!nodes) {
+    return null;
+  }
+
+  return transfromNodesToFolderTree(nodes);
 };
