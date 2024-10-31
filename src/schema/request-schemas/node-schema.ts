002b2b2607533f56bb7c70650c044ce182bfadda
lukasz-mac <luka.koscik@gmail.com>
2024-10-31 16:37:56 +0100

    update create node schema to support file validation

--- src/schema/request-schemas/node-schema.ts
+++ src/schema/request-schemas/node-schema.ts
@@ -1,9 +1,25 @@
 import { NodeType } from "@prisma/client";
 import z from "zod";
 
-export const CreateNodeSchema = z.object({
-  body: z
-    .object({
+const FILE_SIZE_LIMIT = 5242880;
+
+export const CreateNodeSchema = z
+  .object({
+    file: z
+      .object({
+        fieldname: z.string({
+          message: "File has been sent in a wrong format",
+        }),
+        originalname: z.string({ message: "File is missing a name" }),
+        size: z.coerce
+          .number({ message: "File is missing" })
+          .refine((val) => val <= FILE_SIZE_LIMIT, {
+            message: "File cannot exceed size of 5 mb ",
+          }),
+        mimetype: z.string({ message: "File is missing a type" }),
+      })
+      .optional(),
+    body: z.object({
       name: z.string({ message: "Name is required" }),
       type: z.nativeEnum(NodeType, {
         message: "You can only create folders or files",
@@ -11,16 +27,30 @@ export const CreateNodeSchema = z.object({
       parentNodeId: z.coerce.number({
         message: "Uploaded resource does not have parent folder",
       }),
-      fileLink: z.string().optional(),
-      userId: z.coerce.number().optional(),
-      file: z.custom<File>((data) => data instanceof File, {
-        message: "File has been sent in a wrong format",
-      }),
-    })
-    .refine(
-      (data) => {
-        return data.type === NodeType.FILE && data.fileLink;
-      },
-      { message: "Failed to upload file to the server" }
-    ),
-});
+      userId: z.coerce
+        .number({ message: "Missing user of created resource" })
+        .optional(),
+    }),
+  })
+  .refine(
+    (data) => {
+      if (data.body.type === NodeType.FILE) {
+        return data.file;
+      }
+      return true;
+    },
+    {
+      message: "File must is missing",
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.body.type === NodeType.FOLDER) {
+        return !data.file;
+      }
+      return true;
+    },
+    {
+      message: "You cannot upload folder as a file",
+    }
+  );
