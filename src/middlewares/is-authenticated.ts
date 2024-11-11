a9ae3bdb539873963edccf2cbe3e6550a471fffe
lukasz-mac <luka.koscik@gmail.com>
2024-11-11 10:36:13 +0100

    refactor is authenticated to use jwt

--- src/middlewares/is-authenticated.ts
+++ src/middlewares/is-authenticated.ts
@@ -1,12 +1,20 @@
 import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
 
 const isAuthenticated: RequestHandler = (req, res, next) => {
-  if (!!req.user) {
-    next();
+  const authCookie =
+    (req.cookies as {}) && (req.cookies["authCookie"] as string | undefined);
+  if (!authCookie) {
+    res.status(401).json({ message: "Unauthenticated" });
+    return;
   }
-  res
-    .status(401)
-    .json({ error: "You need to be logged in to access this resource" });
+
+  jwt.verify(authCookie, process.env.JWT_SECRET!, (err, _user) => {
+    if (err) {
+      res.status(403).json({ message: "Unauthorized" });
+    }
+    next();
+  });
 };
 
 export default isAuthenticated;
