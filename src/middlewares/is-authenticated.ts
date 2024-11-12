5da8fb5eb25bcb16948063013566c86203b047da
lukasz-mac <luka.koscik@gmail.com>
2024-11-12 16:05:13 +0100

    correct cookie name & change errors in isAuthenticated

--- src/middlewares/is-authenticated.ts
+++ src/middlewares/is-authenticated.ts
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated: RequestHandler = (req, res, next) => {
   const authCookie =
-    (req.cookies as {}) && (req.cookies["authCookie"] as string | undefined);
+    (req.cookies as {}) && (req.cookies["auth_cookie"] as string | undefined);
+  console.log(authCookie);
   if (!authCookie) {
     res.status(401).json({ message: "Unauthenticated" });
     return;
@@ -11,7 +12,7 @@ const isAuthenticated: RequestHandler = (req, res, next) => {
 
   jwt.verify(authCookie, process.env.JWT_SECRET!, (err, _user) => {
     if (err) {
-      res.status(403).json({ message: "Unauthorized" });
+      res.status(401).json({ message: "Incorrect authentication credentials" });
     }
     next();
   });
