c4030c20f975a9873d1f5edf70433c44bf6cf681
lukasz-mac <luka.koscik@gmail.com>
2024-12-16 20:12:44 +0100

    add query to check if specifed node id is within children of sharen folder

---
+++ prisma/sql/queryFolderWithinSharedNode.sql
@@ -0,0 +1,35 @@
+
+ WITH RECURSIVE user_nodes AS (
+  SELECT 
+    node_id, 
+    parent_node_id,
+    type,
+    name,
+    user_id
+  FROM 
+    "nodes"
+  WHERE 
+    node_id = ALL (
+        SELECT links.shared_node_id AS "sharedNodeId"
+        FROM "node_share_links" as links
+        WHERE links.link_hash = $1
+        )
+  UNION 
+    SELECT 
+      r_nodes.node_id,
+      r_nodes.parent_node_id,
+      r_nodes.type,
+      r_nodes.name,
+      r_nodes.user_id
+    FROM 
+      "nodes" r_nodes 
+    INNER JOIN user_nodes r_nodes1 
+      ON r_nodes.parent_node_id = r_nodes1.node_id
+) SELECT
+  node_id "nodeId", 
+  parent_node_id "parentNodeId",
+  name,
+  user_id "userId"
+FROM 
+  user_nodes
+WHERE node_id = $2
