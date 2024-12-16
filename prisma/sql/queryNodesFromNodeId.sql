3a395947c123ad2eabc5600ee6173a496ea39051
lukasz-mac <luka.koscik@gmail.com>
2024-12-16 09:33:15 +0100

    return camel case properties from sql query, return only folder nodes

--- prisma/sql/queryNodesFromNodeId.sql
+++ prisma/sql/queryNodesFromNodeId.sql
@@ -3,7 +3,8 @@ WITH RECURSIVE user_nodes AS (
     node_id, 
     parent_node_id,
     type,
-    name
+    name,
+    user_id
   FROM 
     "nodes"
   WHERE 
@@ -13,12 +14,17 @@ WITH RECURSIVE user_nodes AS (
       r_nodes.node_id,
       r_nodes.parent_node_id,
       r_nodes.type,
-      r_nodes.name
+      r_nodes.name,
+      r_nodes.user_id
     FROM 
       "nodes" r_nodes 
     INNER JOIN user_nodes r_nodes1 
       ON r_nodes.parent_node_id = r_nodes1.node_id
 ) SELECT
-  *
+  node_id "nodeId", 
+  parent_node_id "parentNodeId",
+  name,
+  user_id "userId"
 FROM 
-  user_nodes;
+  user_nodes
+WHERE type = 'FOLDER';
